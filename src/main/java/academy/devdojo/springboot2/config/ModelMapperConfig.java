90c093aa9f5487dfab8f116ecefcc51574e1fb5f
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-26 16:33:54 -0300

    feat: modelmapper config

---
+++ src/main/java/academy/devdojo/springboot2/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package academy.devdojo.springboot2.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}
