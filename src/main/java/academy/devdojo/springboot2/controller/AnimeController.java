b4bdba9721a49ae12735f5e577039f53410c432c
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-26 13:15:01 -0300

    feat: separating the layers

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,8 +1,9 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/anime")
+@RequestMapping("/animes")
 @Log4j2
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AnimeController {
 
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
-    @GetMapping("/list")
+    @GetMapping
     public List<Anime> list() {
-        return List.of(new Anime("Goku"));
+        return animeService.listAll();
     }
 }
