a7de53b1a1888b9707bad8edfbec0002f560161d
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-26 16:35:26 -0300

    feat: add data JPA

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,9 +1,9 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface AnimeRepository {
-    List<Anime> listAll();
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
 }
