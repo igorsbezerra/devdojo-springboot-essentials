588b2da27612c67ed4c97238eb4d472e9c2740e5
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-26 21:02:16 -0300

    feat: exception handler

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,15 +1,14 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -31,7 +30,7 @@ public class AnimeService {
 
     public Anime findByIdOrThrowBadRequestException(Long id) {
         return animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found."));
+                .orElseThrow(() -> new BadRequestException("Anime not found."));
     }
 
     public Anime save(AnimePostRequestBody anime) {
