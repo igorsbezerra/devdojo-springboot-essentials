c9e32f176347ccee9df756bdff974b17f6cea2a2
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-26 21:29:55 -0300

    dependency: add validation in save Anime

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -35,7 +36,7 @@ public class AnimeController {
     }
 
     @PostMapping
-    public ResponseEntity<Anime> create(@RequestBody AnimePostRequestBody animePostRequestBody) {
+    public ResponseEntity<Anime> create(@Valid @RequestBody AnimePostRequestBody animePostRequestBody) {
         return ResponseEntity.status(HttpStatus.CREATED).body(animeService.save(animePostRequestBody));
     }
 
