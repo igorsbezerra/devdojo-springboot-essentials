64400829914d9805a2f7b7a5bb94b4a115ce29af
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-28 00:07:30 -0300

    test: full integration tests

--- src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
+++ src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
@@ -2,7 +2,9 @@ package academy.devdojo.springboot2.integration;
 
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.repository.AnimeRepository;
+import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.util.AnimeCreator;
+import academy.devdojo.springboot2.util.AnimePostRequestBodyCreator;
 import academy.devdojo.springboot2.wrapper.PageableResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -12,12 +14,17 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class AnimeControllerIT {
 
     @Autowired
@@ -39,5 +46,101 @@ class AnimeControllerIT {
         Assertions.assertThat(anime).isNotNull();
         Assertions.assertThat(anime.toList()).isNotEmpty().hasSize(1);
         Assertions.assertThat(anime.toList().get(0).getName()).isEqualTo(expectedName);
+    }
+
+    @Test
+    @DisplayName("listAll returns List of anime when successfully")
+    void listAll_ReturnsListOfAnime_whenSuccessfully() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createValidAnime());
+        String expectedName = savedAnime.getName();
+
+        List<Anime> animes = testRestTemplate.exchange(
+                "/animes/all", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Anime>>() {
+                }).getBody();
+
+        Assertions.assertThat(animes).isNotNull().isNotEmpty().hasSize(1);
+        Assertions.assertThat(animes.get(0).getName()).isEqualTo(expectedName);
+    }
+
+    @Test
+    @DisplayName("findById returns anime when successfully")
+    void findById_ReturnsAnime_whenSuccessfully() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createValidAnime());
+        Long expectedId = savedAnime.getId();
+
+        Anime anime = testRestTemplate.getForObject(
+                "/animes/{id}",
+                Anime.class,
+                expectedId
+        );
+
+        Assertions.assertThat(anime).isNotNull();
+        Assertions.assertThat(anime.getId()).isNotNull().isEqualTo(expectedId);
+    }
+
+    @Test
+    @DisplayName("findByName returns a list of anime when successfully")
+    void findByName_ReturnsListOfAnime_whenSuccessfully() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createValidAnime());
+        String expectedName = savedAnime.getName();
+        String url = String.format("/animes/find?name=%s", expectedName);
+
+        List<Anime> animes = testRestTemplate.exchange(
+                url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Anime>>() {
+                }).getBody();
+
+        Assertions.assertThat(animes).isNotNull().isNotEmpty().hasSize(1);
+        Assertions.assertThat(animes.get(0).getName()).isEqualTo(expectedName);
+    }
+
+    @Test
+    @DisplayName("findByName returns an empty list when anime is not found")
+    void findByName_ReturnsEmptyListOfAnime_whenAnimeIsNotFound() {
+        List<Anime> animes = testRestTemplate.exchange(
+                "/animes/find?name=invalid-name", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Anime>>() {
+                }).getBody();
+
+        Assertions.assertThat(animes).isNotNull().isEmpty();
+    }
+
+    @Test
+    @DisplayName("create returns anime when successfully")
+    void create_ReturnsAnime_whenSuccessfully() {
+        AnimePostRequestBody animePostRequestBody = AnimePostRequestBodyCreator.createAnimePostRequestBody();
+
+        ResponseEntity<Anime> animeResponseEntity = testRestTemplate.postForEntity("/animes", animePostRequestBody, Anime.class);
+
+        Assertions.assertThat(animeResponseEntity).isNotNull();
+        Assertions.assertThat(animeResponseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        Assertions.assertThat(animeResponseEntity.getBody()).isNotNull();
+        Assertions.assertThat(animeResponseEntity.getBody().getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("replace update anime when successfully")
+    void replace_UpdateAnime_whenSuccessfully() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createValidAnime());
+        savedAnime.setName("new-name");
+
+        ResponseEntity<Void> animeResponseEntity = testRestTemplate.exchange("/animes",
+                HttpMethod.PUT, new HttpEntity<>(savedAnime), Void.class);
+
+        Assertions.assertThat(animeResponseEntity).isNotNull();
+        Assertions.assertThat(animeResponseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    @DisplayName("delete anime when successfully")
+    void delete_RemoveAnime_whenSuccessfully() {
+        Anime savedAnime = animeRepository.save(AnimeCreator.createValidAnime());
+
+        ResponseEntity<Void> animeResponseEntity = testRestTemplate.exchange("/animes/{id}",
+                HttpMethod.DELETE, null, Void.class, savedAnime.getId());
+
+        Assertions.assertThat(animeResponseEntity).isNotNull();
+        Assertions.assertThat(animeResponseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 }
